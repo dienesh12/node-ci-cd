e0dab766363c9703d23558e8ae0f9fd115c3f581
dienesh12 <dineshpathakhere@gmail.com>
2024-01-24 20:53:44 +0530

    test webhook

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded())
 
 app.get('/', (req, res)  => {
-    res.json({ "success": "ok" })
+    res.json({ "success": "true" })
 })
 
 app.listen(PORT, () => {
